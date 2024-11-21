6e4e8f51100046e9532f447f7fbe2ab6a247452c
Nfx1z <mahadikanafiz86@gmail.com>
2024-11-22 01:22:12 +0700

    Completing file model and file controller, also setup the right routes

---
+++ src/controllers/fileController.py
@@ -0,0 +1,57 @@
+from flask import jsonify
+from src.model.fileModel import (
+    upload_file, 
+    replace_file,
+    rename_file,
+    delete_file
+)
+
+# Define routes for file upload, replace, rename, and delete
+def upload_file_controllers(request_json):
+    bucket_name = request_json.get("bucket_name")
+    file_name = request_json.get("file_name")
+    folder_name = request_json.get("folder_name")
+    # upload file
+    isValid, message = upload_file(bucket_name, file_name, folder_name)
+    # failed to upload file
+    if not isValid:
+        return jsonify({"status_code": 400, "error": message}), 400
+    # successful
+    return jsonify({"status_code": 201, "message": message}), 201
+
+def replace_file_controllers(request_json):
+    bucket_name = request_json.get("bucket_name")
+    file_name = request_json.get("file_name")
+    folder_name = request_json.get("folder_name")
+    # replace file
+    isValid, message = replace_file(bucket_name, file_name, folder_name)
+    # failed to replace file
+    if not isValid:
+        return jsonify({"status_code": 400, "error": message}), 400
+    # successful
+    return jsonify({"status_code": 200, "message": message}), 200
+
+def rename_file_controllers(request_json):
+    bucket_name = request_json.get("bucket_name")
+    folder_name = request_json.get("folder_name")
+    old_filename = request_json.get("old_filename")
+    new_filename = request_json.get("new_filename")
+    # rename file
+    isValid, message = rename_file(bucket_name, folder_name, old_filename, new_filename)
+    # failed to rename file
+    if not isValid:
+        return jsonify({"status_code": 400, "error": message}), 400
+    # successful
+    return jsonify({"status_code": 200, "message": message}), 200
+
+def delete_file_controllers(request_json):
+    bucket_name = request_json.get("bucket_name")
+    folder_name = request_json.get("folder_name")
+    file_name = request_json.get("file_name")
+    # delete file
+    isValid, message = delete_file(bucket_name, folder_name, file_name)
+    # failed to delete file
+    if not isValid:
+        return jsonify({"status_code": 400, "error": message}), 400
+    # successful
+    return jsonify({"status_code": 200, "message": message}), 200
--- src/model/bucketModel.py
+++ src/model/bucketModel.py
@@ -1,5 +1,6 @@
-import os
-import sys
+import json, os, sys
+from flask import jsonify
+from collections import defaultdict
 from google.cloud import storage
 from google.auth.exceptions import DefaultCredentialsError
 from google.cloud.exceptions import NotFound
@@ -9,67 +10,110 @@ current_dir = os.path.dirname(os.path.abspath(__file__))
 parent_dir = os.path.dirname(current_dir)
 sys.path.append(parent_dir)
 
-# Import BUCKET_NAME, BUCKET_LOCATION from the variables.py file
-from variables import BUCKET_NAME, BUCKET_LOCATION
+# Import PROJECT_ID from the variables.py file
+from variables import PROJECT_ID
+
+# Function to list buckets with details
+def list_buckets_with_details():
+    # Initialize the Google Cloud Storage client
+    client = storage.Client()
+    # List all buckets in your project
+    buckets = client.list_buckets()
+    # Iterate through the buckets and print their details
+    response = []
+    for bucket in buckets:
+        response.append({
+            "bucket_name": bucket.name,
+            "location": bucket.location,
+            "storage_class": bucket.storage_class,
+            "created": bucket.time_created,
+        })
+        
+    # Close the client
+    client.close()
+
+    return jsonify({f'Buckets in "{PROJECT_ID}" ': response})
 
 # Function to check if a bucket exists
-def check_bucket_exists():
+def check_bucket_exists(bucket_name):
     # Initialize the storage client using default credentials (from environment variable)
     storage_client = storage.Client()
     # Check if the bucket exists
     try:
-        storage_client.get_bucket(BUCKET_NAME)
-        print(f"Bucket '{BUCKET_NAME}' exists.")
+        storage_client.get_bucket(bucket_name)
         return True  # Bucket exists
     except NotFound:
-        print(f"Bucket '{BUCKET_NAME}' does not exist.")
         return False  # Bucket does not exist
 
 # Function to create a new Google Cloud Storage bucket
-def create_bucket():
+def create_gcs_bucket(bucket_name, bucket_location):
     try:
         # Initialize the storage client using default credentials (from environment variable)
         storage_client = storage.Client()
         # Create a new bucket instance
-        bucket = storage_client.bucket(BUCKET_NAME)
+        bucket = storage_client.bucket(bucket_name)
         # Set the location for the bucket (optional)
-        bucket.create = BUCKET_LOCATION
+        bucket.location = bucket_location
         # Create the bucket
         bucket = storage_client.create_bucket(bucket)  # This will create the bucket
-        print(f"Bucket {bucket.name} created in {bucket.location} location.")
-        
+        return True, f"Bucket {bucket.name} created in {bucket.location} location."
     # Handle exceptions
     except DefaultCredentialsError:
-        print("Error: Could not authenticate. Please check your GOOGLE_APPLICATION_CREDENTIALS environment variable.")
+        return False, "Could not authenticate. Please check your GOOGLE_APPLICATION_CREDENTIALS environment variable."
     except Exception as e:
-        print(f"Error creating bucket: {e}")
-
-    # Close the client
-    storage_client.close()
+        return False, f"Error creating bucket: {e}"
+    finally:
+        # Close the client
+        storage_client.close()
 
 # Function to delete a Google Cloud Storage bucket
-def delete_gcs_bucket():
-    storage_client = storage.Client()
-
+def delete_gcs_bucket(bucket_name):
     try:
-        bucket = storage_client.bucket(BUCKET_NAME)
+        # Initialize the storage client using default credentials (from environment variable)
+        storage_client = storage.Client()
+        # Get the bucket
+        bucket = storage_client.bucket(bucket_name)
 
         # List and delete all blobs in the bucket
         blobs = bucket.list_blobs()
         for blob in blobs:
             blob.delete()
-            print(f"Blob '{blob.name}' deleted.")
 
         # Now delete the bucket
         bucket.delete()
-        print(f"Bucket '{BUCKET_NAME}' deleted successfully.")
+        return True, f"Bucket '{bucket_name}' deleted successfully."
     except NotFound:
-        print(f"Bucket '{BUCKET_NAME}' not found.")
+        return False, f"Bucket '{bucket_name}' not found."
     except Exception as e:
-        print(f"Error occurred: {e}")
+        return False, f"Error occurred: {e}"
+    finally:
+        # Close the client
+        storage_client.close()
 
-    # Close the client
-    storage_client.close()
+# Function to list files and folders in a Google Cloud Storage bucket
+def list_files_and_folders(bucket_name):
+    # Initialize the GCS client
+    client = storage.Client()
+    # To store the tree-like structure of files and folders
+    tree = defaultdict(list)
+    # List all objects in the bucket
+    blobs = client.list_blobs(bucket_name)
+    
+    # Organize the objects into a tree-like structure
+    for blob in blobs:
+        # Split the file path by '/' to create the tree structure
+        parts = blob.name.split('/')
+        folder = parts[0] + '/' # First part is the folder
+        if len(parts) > 1:
+            tree[folder].append('/'.join(parts[1:]))  # Append subfiles or subfolders
+    
+    # Convert defaultdict to a regular dict for JSON serialization
+    return dict(tree)
 
-if not check_bucket_exists():
-    create_bucket()
+# Function to convert the tree structure to a JSON response
+def json_response(bucket_name):
+    # Get the file and folder tree structure
+    tree = list_files_and_folders(bucket_name)
+    
+    # Convert the tree structure to a JSON response (string)
+    return json.dumps(tree, indent=4)
