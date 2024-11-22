a7fc5ba473292f00c1b147607fe732223f10d9c9
Nfx1z <mahadikanafiz86@gmail.com>
2024-11-22 15:37:47 +0700

    Add a rate limiter

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # Cloud Storage Flask API
-**A simple Flask API for managing files and folders in Google Cloud Storage.**
+A simple Flask API for managing files and folders in Google Cloud Storage.**
 
+**Build with : Python 3.13**
 
 ## Installation
 1. Clone this repository.
@@ -10,125 +11,127 @@
 5. Run the Flask development server with `flask --debug run -h 0.0.0.0`.
 
 ## API Endpoints
-> ---
->### Bucket Management
->- **GET /buckets**
->> List all buckets with details.
->>
->- **POST /bucket/create**
->> Create a new bucket.  
->> **Request Body:**  
->> ```json
->> {
->>   "bucket_name": "your-bucket-name",
->>   "region": "bucket-region"
->> }
->> ```
->>
-> - **DELETE /bucket/delete**
->> Delete a bucket.  
->>  **Request Body:**  
->>  ```json
->>  {
->>    "bucket_name": "your-bucket-name"
->>  }
->>  ```
-> ---
->### File Management
-> - **GET /files**
->> Get all items from the bucket
->> **Request body**
->> ```json
->> {
->>    "bucket_name": "your-bucket-name"
->> }
->> ```
->- **POST /file/upload**  
->>  Upload a new file to a bucket.  
->>  **Request Body:**  
->>  ```json
->>  {
->>    "bucket_name": "your-bucket-name",
->>    "folder_name": "your-folder-name",
->>    "file_name": "your-file-name"
->>  }
->>  ```
->> 
-> - **PUT /file/replace**  
->>  Replace an existing file in a bucket.  
->>  **Request Body:**  
->>  ```json
->>  {
->>    "bucket_name": "your-bucket-name",
->>    "folder_name": "your-folder-name",
->>    "file_name": "your-file-name"
->>  }
->>  ```
->>
->- **PUT /file/rename**  
->>  Rename a file in a bucket.  
->>  **Request Body:**  
->>  ```json
->>  {
->>    "bucket_name": "your-bucket-name",
->>    "folder_name": "your-folder-name",
->>    "old_filename": "old-file-name",
->>    "new_filename": "new-file-name"
->>  }
->>  ```
->>
->- **DELETE /file/delete**  
->>  Delete a file from a bucket.  
->>  **Request Body:**  
->>  ```json
->>  {
->>    "bucket_name": "your-bucket-name",
->>    "folder_name": "your-folder-name",
->>    "file_name": "your-file-name"
->>  }
->>  ```
-> ---
->### Folder Management
->>
->- **POST /folder/create**  
->>  Create a new folder in a bucket.  
->>  **Request Body:**  
->>  ```json
->>  {
->>    "bucket_name": "your-bucket-name",
->>    "folder_name": "your-folder-name"
->>  }
->>  ```
->>
->- **PUT /folder/rename**  
->>  Rename a folder in a bucket.  
->>  **Request Body:**  
->>  ```json
->>  {
->>    "bucket_name": "your-bucket-name",
->>    "old_foldername": "old-folder-name",
->>    "new_foldername": "new-folder-name"
->>  }
->>  ```
->>
-> ### ⚠️ WARNING
-> ##### when delete nested folders, you must keep in mind that it will delete all folders if it has no file in it.
->- **DELETE /folder/delete**  
->>  Delete a subfolder from a bucket. 
->>  **Request Body:**  
->>  ```json
->>  {
->>    "bucket_name": "your-bucket-name",
->>    "parent_folder": "parent-folder-name",
->>    "subfolder_name": "sub-folder-name"
->>  }
->>  ```
-> ---
+ ---
+### Bucket Management
+- **GET /buckets**
+    List all buckets with details.
+
+- **POST /bucket/create**
+    Create a new bucket with standard storage.
+    ##### Request Body (JSON):  
+    ```json
+    {
+        "bucket_name": "your-bucket-name",
+        "region": "bucket-region"
+    }
+    ```
+
+- **DELETE /bucket/delete**
+    Delete a bucket.  
+    ##### Request Body (JSON):  
+    ```json
+    {
+        "bucket_name": "your-bucket-name"
+    }
+    ```
+---
+### File Management
+- **GET /files**
+    Get all items from the bucket
+    ##### Request Body (JSON):  
+    ```json
+    {
+        "bucket_name": "your-bucket-name"
+    }
+    ```
+- **POST /file/upload**  
+    Upload a new file to a bucket.  
+    ##### Request Body (JSON):  
+    ```json
+    {
+        "bucket_name": "your-bucket-name",
+        "folder_name": "your-folder-name",
+        "file_name": "your-file-name"
+    }
+    ```
+ 
+- **PUT /file/replace**  
+    Replace an existing file in a bucket.
+    ##### Request Body (JSON):  
+    ```json
+    {
+        "bucket_name": "your-bucket-name",
+        "folder_name": "your-folder-name",
+        "file_name": "your-file-name"
+    }
+    ```
+
+- **PUT /file/rename**  
+    Rename a file in a bucket.  
+    ##### Request Body (JSON):
+    ```json
+    {
+        "bucket_name": "your-bucket-name",
+        "folder_name": "your-folder-name",
+        "old_filename": "old-file-name",
+        "new_filename": "new-file-name"
+    }
+    ```
+
+- **DELETE /file/delete**  
+    Delete a file from a bucket.  
+    ##### Request Body (JSON): 
+    ```json
+    {
+        "bucket_name": "your-bucket-name",
+        "folder_name": "your-folder-name",
+        "file_name": "your-file-name"
+    }
+    ```
+ ---
+### Folder Management
+
+- **POST /folder/create**  
+  Create a new folder in a bucket.  
+  **Request Body (JSON):**  
+  ```json
+  {
+    "bucket_name": "your-bucket-name",
+    "folder_name": "your-folder-name"
+  }
+  ```
+
+- **PUT /folder/rename**  
+  Rename a folder in a bucket.  
+  **Request Body (JSON):**  
+  ```json
+  {
+    "bucket_name": "your-bucket-name",
+    "old_foldername": "old-folder-name",
+    "new_foldername": "new-folder-name"
+  }
+  ```
+
+!!!  WARNING
+    ##### when delete nested folders, you must keep in mind that it will delete all folders if it has no file in it.
+ 
+- **DELETE /folder/delete**  
+  Delete a subfolder from a bucket. 
+  **Request Body (JSON):**  
+  ```json
+  {
+    "bucket_name": "your-bucket-name",
+    "parent_folder": "parent-folder-name",
+    "subfolder_name": "sub-folder-name"
+  }
+  ```
+
 ## Environment Variables
 
 The following environment variables are required:
 
  - `GOOGLE_APPLICATION_CREDENTIALS`: The path to the service account key file.
+ - `FLASK_APP`: main.py. The main file of the Flask app.
 
 ## Development
 
--- src/routes.py
+++ src/routes.py
@@ -1,4 +1,5 @@
-from flask import Blueprint, jsonify, request
+from flask import Blueprint, request, jsonify
+from src.rateLimiter import is_rate_limited
 from src.controllers.bucketController import (
     create_bucket_controllers,
     delete_bucket_controllers,
@@ -19,70 +20,145 @@ from src.controllers.folderController import (
 # Create a Blueprint for the routes
 routes = Blueprint('routes', __name__)
 
+# Example route with rate limiting
+@routes.route('/test', methods=['GET'])
+def get_resource():
+    ip_address = request.remote_addr  # Get the client's IP address
+    #  Check if the IP address is rate limited
+    if is_rate_limited(ip_address):
+        return jsonify({"error": "Rate limit exceeded. Try again later."}), 429
+    # If not rate limited, proceed with the request
+    return jsonify({"data": "Here is your resource!"})
+
 # ==============================================================================
 # List all buckets
 @routes.route('/buckets', methods=['GET'])
 def get_buckets_route():
+    ip_address = request.remote_addr  # Get the client's IP address
+    #  Check if the IP address is rate limited
+    if is_rate_limited(ip_address):
+        return jsonify({"error": "Rate limit exceeded. Try again later."}), 429
+    # If not rate limited, proceed with the request
     return list_buckets_with_details_controllers()
 
 # Create a new bucket
 @routes.route('/bucket/create', methods=['POST'])
 def create_bucket_route():
+    # Get the JSON data from the request
     request_json = request.get_json()
+    ip_address = request.remote_addr  # Get the client's IP address
+    #  Check if the IP address is rate limited
+    if is_rate_limited(ip_address):
+        return jsonify({"error": "Rate limit exceeded. Try again later."}), 429
+    # If not rate limited, proceed with the request
     return create_bucket_controllers(request_json)
 
 # Delete a bucket
 @routes.route('/bucket/delete', methods=['DELETE'])
 def delete_bucket_route():
+    # Get the JSON data from the request
     request_json = request.get_json()
+    ip_address = request.remote_addr  # Get the client's IP address
+    #  Check if the IP address is rate limited
+    if is_rate_limited(ip_address):
+        return jsonify({"error": "Rate limit exceeded. Try again later."}), 429
+    # If not rate limited, proceed with the request
     return delete_bucket_controllers(request_json)
 
 # List all objects in a bucket
 @routes.route('/files', methods=['GET'])
 def bucket_list_route():
+    # Get the JSON data from the request
     request_json = request.get_json()
+    ip_address = request.remote_addr  # Get the client's IP address
+    #  Check if the IP address is rate limited
+    if is_rate_limited(ip_address):
+        return jsonify({"error": "Rate limit exceeded. Try again later."}), 429
+    # If not rate limited, proceed with the request
     return list_objects_controllers(request_json)
 
 # ==============================================================================
 # Upload a new file to a bucket
 @routes.route('/file/upload', methods=['POST'])
 def upload_file_route():
+    # Get the JSON data from the request
     request_json = request.get_json()
+    ip_address = request.remote_addr  # Get the client's IP address
+    #  Check if the IP address is rate limited
+    if is_rate_limited(ip_address):
+        return jsonify({"error": "Rate limit exceeded. Try again later."}), 429
+    # If not rate limited, proceed with the request
     return upload_file_controllers(request_json)
 
 # Replace a file in a bucket
 @routes.route('/file/replace', methods=['PUT'])
 def replace_file_route():
+    # Get the JSON data from the request
     request_json = request.get_json()
+    ip_address = request.remote_addr  # Get the client's IP address
+    #  Check if the IP address is rate limited
+    if is_rate_limited(ip_address):
+        return jsonify({"error": "Rate limit exceeded. Try again later."}), 429
+    # If not rate limited, proceed with the request
     return replace_file_controllers(request_json)
 
 # Rename a file in a bucket
 @routes.route('/file/rename', methods=['PUT'])
 def rename_file_route():
+    # Get the JSON data from the request
     request_json = request.get_json()
+    ip_address = request.remote_addr  # Get the client's IP address
+    #  Check if the IP address is rate limited
+    if is_rate_limited(ip_address):
+        return jsonify({"error": "Rate limit exceeded. Try again later."}), 429
+    # If not rate limited, proceed with the request
     return rename_file_controllers(request_json)
 
 # Delete a file from a bucket
 @routes.route('/file/delete', methods=['DELETE'])
 def delete_file_route():
+    # Get the JSON data from the request
     request_json = request.get_json()
+    ip_address = request.remote_addr  # Get the client's IP address
+    #  Check if the IP address is rate limited
+    if is_rate_limited(ip_address):
+        return jsonify({"error": "Rate limit exceeded. Try again later."}), 429
+    # If not rate limited, proceed with the request
     return delete_file_controllers(request_json)
 
 # ==============================================================================
 # Create a new folder in a bucket
 @routes.route('/folder/create', methods=['POST'])
 def create_folder_route():
+    # Get the JSON data from the request
     request_json = request.get_json()
+    ip_address = request.remote_addr  # Get the client's IP address
+    #  Check if the IP address is rate limited
+    if is_rate_limited(ip_address):
+        return jsonify({"error": "Rate limit exceeded. Try again later."}), 429
+    # If not rate limited, proceed with the request
     return create_nested_folders(request_json)
 
 # Rename a folder in a bucket
 @routes.route('/folder/rename', methods=['PUT'])
 def rename_folder_route():
+    # Get the JSON data from the request
     request_json = request.get_json()
+    ip_address = request.remote_addr  # Get the client's IP address
+    #  Check if the IP address is rate limited
+    if is_rate_limited(ip_address):
+        return jsonify({"error": "Rate limit exceeded. Try again later."}), 429
+    # If not rate limited, proceed with the request
     return rename_nested_folders(request_json)
 
 # Delete a folder in a bucket
 @routes.route('/folder/delete', methods=['DELETE'])
 def delete_folder_route():
+    # Get the JSON data from the request
     request_json = request.get_json()
+    ip_address = request.remote_addr  # Get the client's IP address
+    #  Check if the IP address is rate limited
+    if is_rate_limited(ip_address):
+        return jsonify({"error": "Rate limit exceeded. Try again later."}), 429
+    # If not rate limited, proceed with the request
     return delete_nested_folders(request_json)
